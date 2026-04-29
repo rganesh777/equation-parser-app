050a3c871b37f5db05e11e00c1bcb01cfc14b8aa
Ganesh Rajendran <rganesh777@gmail.com>
2026-04-29 17:02:57 +1000

    updated base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/equation-parser/",
+  base: "/equation-parser-app/",
   plugins: [react()],
   test: {
     environment: "jsdom",
